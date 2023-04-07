cf569d595aad155f23ab91f6dd0558a4eeedf622
kumaradji <kumaradji@me.com>
2023-04-07 12:24:23 +0300

    добавлена пагинация по 10 отдельная страница поиск написать новость при сохранении ImproperlyConfigured at /news/create/

--- news_portal/news/models.py
+++ news_portal/news/models.py
@@ -1,3 +1,5 @@
+from audioop import reverse
+
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models import Sum
@@ -73,12 +75,13 @@ class Post(models.Model):
         self.save()
 
     def __str__(self):
-        return f'{self.postCategory}'
+        return f'{self.title}'
 
 
 class PostCategory(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    related_name = 'post'
 
     class Meta:
         verbose_name = 'Категория новости'
@@ -87,6 +90,8 @@ class PostCategory(models.Model):
     def __str__(self):
         return f'{self.post} ({self.category})'
 
+    def get_absolute_url(self):
+        return reverse('post_detail', args=[str(self.id)])
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
--- news_portal/news/views.py
+++ news_portal/news/views.py
@@ -1,7 +1,11 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import (
+    ListView, DetailView, CreateView, UpdateView, DeleteView
+)
 
 from .filters import PostFilter
+from .forms import PostForm
 from .models import *
+from django.urls import reverse_lazy
 
 
 class PostList(ListView):
@@ -9,6 +13,17 @@ class PostList(ListView):
     ordering = '-dateCreation'
     template_name = 'news.html'
     context_object_name = 'news'
+    paginate_by = 10
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = PostFilter(self.request.GET, queryset)
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filterset'] = self.filterset
+        return context
 
 
 class PostSearch(ListView):
@@ -33,3 +48,23 @@ class PostDetail(DetailView):
     template_name = 'post_view.html'
     slug_url_kwarg = 'post_slug'
     context_object_name = 'post'
+
+
+class PostCreate(CreateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'post_edit.html'
+
+
+# Добавляем представление для изменения товара.
+class PostUpdate(UpdateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'post_edit.html'
+
+
+# Представление удаляющее товар.
+class PostDelete(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('post_list')
