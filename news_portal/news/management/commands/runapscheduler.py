2af5bf7640684637e8703d168d07043526d913d1
kumaradji <kumaradji@gmail.com>
2023-06-22 13:16:59 +0300

    переключатель времени работает

--- news_portal/news/management/commands/runapscheduler.py
+++ news_portal/news/management/commands/runapscheduler.py
@@ -11,7 +11,7 @@ from django_apscheduler import util
 from django_apscheduler.jobstores import DjangoJobStore
 from django_apscheduler.models import DjangoJobExecution
 
-from news.models import Post, Category
+from news_portal.news.models import Post, Category
 
 logger = logging.getLogger(__name__)
 
